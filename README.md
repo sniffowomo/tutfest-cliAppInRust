ed672f8a5a4957bd4b034ac57eab99824406a041
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-01-27 17:41:18 +0000

    📚 : Tut Cli Apps in Russt

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tutfest-cliAppInRust
 📚 https://rust-cli.github.io/book/index.html
+
+
+> Badongo
+> Write shit to pevent shutdown on rubbisho 
+
+ gfgfgfgtt
--- s/in.sh
+++ s/in.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/codespace/.profile
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/codespace/.profile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
