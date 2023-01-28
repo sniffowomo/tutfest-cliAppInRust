d5c4efd85fb9dd6ce3fe44d66a13c6158b36fc62
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-01-28 09:04:17 +0000

    🔖 : at/a1

--- README.md
+++ README.md
@@ -3,9 +3,22 @@
 
 ----
 1. [What ?](#what-)
+2. [Dir Structure](#dir-structure)
 
 ----
 
 # What ?
 
-1. 
+1. Work Book for - [Writing CLI Apps in Rust](https://rust-cli.github.io/book/index.html)
+   1. Main Reference
+2. And another reference for this - [Lets Get Rusty - Writing CLI Apps](https://youtu.be/XYkiwsplDTg) 
+   1.  Secondary reference 
+
+# Dir Structure 
+
+- Will list out the directories in this repo, since often loose interest by watching only fans ladies wanting to suqirt on my face 
+
+N | ? 
+|:--:|:--:|
+[`sc1`](./sc1/) | System commands in Rust
+
---
+++ sc1/src/main.rs
@@ -0,0 +1,34 @@
+/*
+System commands executed in rust
+*/
+
+use ::std::process::Command;
+use ::yansi::{Color, Paint, Style};
+
+// Defining the colors shit here
+let di_1 = Style::new(Color::Green).bold().bg(Color::Magenta);
+
+fn main() {
+    bn_1();
+    s_1();
+}
+
+// Banner Code here
+fn bn_1() {
+    let di_1 = Style::new(Color::Green).bold().bg(Color::Magenta);
+    println!(
+        "{}",
+        di_1.paint(
+            "
+System Commands with Pipes
+**************************"
+        )
+    )
+}
+
+// System Commands block
+fn s_1() {
+    let mut exa_o1 = Command::new("exa");
+    exa_o1.args(["-la", "--icons"]).status();
+    print!("{:?}", exa_o1);
+}
